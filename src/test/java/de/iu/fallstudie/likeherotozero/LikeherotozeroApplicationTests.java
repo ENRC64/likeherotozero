636d36e5e447af67e59140e284c57afa0377a141
ENRC64 <flach.enrico@gmail.com>
2026-01-25 14:55:33 +0100

    first commit

---
+++ src/test/java/de/iu/fallstudie/likeherotozero/LikeherotozeroApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iu.fallstudie.likeherotozero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeherotozeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
